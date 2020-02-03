e70e834083efd01e64ed914aa74ba4a4d1ec8752
ae23358 <cs.park@gsshop.com>
2020-02-03 10:26:11 +0900

    .

--- src/config/index.js
+++ src/config/index.js
@@ -3,4 +3,7 @@ const ENV = process.env.NODE_ENV || "development";
 const config = {};
 config.ENV = ENV;
 config.api_host = 'http://a439a8e2.ngrok.io';
+// config.api_host = 'http://localhost:4000';
+// config.api_host = 'http://10.52.124.31:4000';
+
 module.exports = config;
